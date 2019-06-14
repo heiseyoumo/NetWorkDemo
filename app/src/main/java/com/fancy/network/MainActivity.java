a560d2f1cd0c8cb68f91a67304f6bc4bd429f96d
fancy <274737258@qq.com>
2019-06-14 13:28:47 +0800

    优化代码

--- app/src/main/java/com/fancy/network/MainActivity.java
+++ app/src/main/java/com/fancy/network/MainActivity.java
@@ -4,32 +4,30 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.fancy.network.bean.OrderCountBean;
+import com.fancy.network.bean.JsonBean;
 import com.fancy.network.callback.HttpResponseCallback;
 import com.fancy.network.util.HttpHelper;
 
 public class MainActivity extends AppCompatActivity {
     Handler handler = new Handler();
+    String url = "http://c.3g.163.com/photo/api/set/0001%2F2250173.json";
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HttpHelper.getInstance().getUrl("getUrl", new HttpResponseCallback<OrderCountBean>() {
-
+                HttpHelper.getInstance().getUrl(url, new HttpResponseCallback<JsonBean>() {
                     @Override
-                    public void onSuccess(OrderCountBean orderCountBean) {
-                        OrderCountBean.DataBean data = orderCountBean.getData();
-                        if (orderCountBean.getRespCode().equals("0000")) {
-                            Toast.makeText(MainActivity.this, "成功", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(MainActivity.this, "接口请求失败", Toast.LENGTH_SHORT).show();
-                        }
+                    public void onSuccess(JsonBean jsonBean) {
+                        textView.setText(jsonBean.getDesc() + ",创造者:" + jsonBean.getCreator());
                     }
 
                     @Override
--- app/src/main/java/com/fancy/network/processor/OkHttpProcessor.java
+++ app/src/main/java/com/fancy/network/processor/OkHttpProcessor.java
@@ -5,6 +5,14 @@ import android.os.Looper;
 
 import com.fancy.network.callback.ICallback;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * @author pengkuanwang
  * @date 2019-06-14
@@ -22,25 +30,42 @@ public class OkHttpProcessor implements IProcessor {
             "    }";
 
     private Handler handler = new Handler(Looper.getMainLooper());
+    OkHttpClient okHttpClient;
+
+    public OkHttpProcessor() {
+        okHttpClient = new OkHttpClient();
+    }
+
 
     @Override
     public void getUrl(String url, final ICallback callback) {
-        new Thread(new Runnable() {
+        final Request request = new Request.Builder()
+                .url(url)
+                .get()//默认就是GET请求，可以不写
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
             @Override
-            public void run() {
-                try {
-                    Thread.sleep(300);
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            callback.onSuccess(json);
-                        }
-                    });
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            public void onFailure(Call call, final IOException e) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.onFail(e.getMessage());
+                    }
+                });
             }
-        }).start();
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String result = response.body().toString();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.onSuccess(result);
+                    }
+                });
+            }
+        });
     }
 
     @Override
